82f9fc8ad7e86bd8a6622e080b5ace6917302179
Alumno <Alumno@BRII-RIVAS>
2022-07-05 23:13:46 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio2Dbrisarivas/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace PSeInt
+{
+	class ejercicio2debrisarivas
+	{
+
+		static void Main(string[] args)
+		{
+			string acutangulo;
+			double ladoa;
+			double ladob;
+			double ladoc;
+			string obtusangulo;
+			string rectangulo;
+			Console.Write("ladoA es");
+			ladoa = Double.Parse(Console.ReadLine());
+			Console.Write("ladoB es");
+			ladob = Double.Parse(Console.ReadLine());
+			Console.Write("ladoC es");
+			ladoc = Double.Parse(Console.ReadLine());
+			if (Math.Pow(ladoa, 2) == Math.Pow(ladob, 2) + Math.Pow(ladoc, 2))
+			{
+				Console.WriteLine(Math.Pow(ladoa, 2) + ":" + Math.Pow(ladob, 2) + ":" + Math.Pow(ladoc, 2) + rectangulo);
+				if (Math.Pow(ladoa, 2) > Math.Pow(ladob, 2) + Math.Pow(ladoc, 2))
+				{
+					Console.WriteLine(Math.Pow(ladoa, 2) + ":" + Math.Pow(ladob, 2) + ":" + ladoc + obtusangulo);
+					if (Math.Pow(ladoa, 2) < Math.Pow(ladob, 2) + Math.Pow(ladoc, 2))
+					{
+						Console.WriteLine(Math.Pow(ladoa, 2) + ":" + Math.Pow(ladob, 2) + ":" + ladoc + acutangulo);
+					}
+					else
+					{
+						Console.WriteLine("error");
+					}
+				}
+				else
+				{
+				}
+			}
+			else
+			{
+			}
+		}
+
+	}
+
+}
+
